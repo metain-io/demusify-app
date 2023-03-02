4d4d70af20c5116acbf8a0a651946dcd4dff8748
Quang Doan <quang.doan@metain.io>
2023-03-02 18:34:59 +0700

    - update header style of page maintenance

--- frontend/src/modules/app/layouts/main-layout-3/index.tsx
+++ frontend/src/modules/app/layouts/main-layout-3/index.tsx
@@ -9,7 +9,7 @@ const MainLayout3 = (props: MainLayout3Props) => {
 
     return (
         <div className="overflow-x-hidden font-body text-jacarta-500">
-            <header className="js-page-header page-header--transparent fixed top-0 z-20 w-full backdrop-blur transition-colors">
+            <header className="js-page-header page-header--transparent fixed top-0 z-20 w-full bg-white/[.15] backdrop-blur transition-colors">
                 <NavigationBar3 />
             </header>
 

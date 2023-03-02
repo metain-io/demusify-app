95f1a5fde7e005c5bc019c60de169d413cb906f7
VTTruong287 <truong.vt287@gmail.com>
2023-03-02 10:10:02 +0700

    [SolHackathon] Update

--- frontend/src/modules/app/index.tsx
+++ frontend/src/modules/app/index.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
 }
--- frontend/src/modules/app/pages/home/index.tsx
+++ frontend/src/modules/app/pages/home/index.tsx
@@ -1,7 +1,44 @@
+import React from 'react';
 import type { NextPage } from 'next';
+import Masonry from 'components/masonry';
+import ReactTable from 'components/react-table';
+// import Masonry from '@mui/lab/Masonry';
 
 const Home: NextPage = () => {
-    return <div>Welcome to Demusify !!!</div>;
+    return (
+        <>
+            <h1>Hello world</h1>
+            <ReactTable />
+            <Masonry />
+            {/* <Masonry columns={4} spacing={2}>
+                {[
+                    'http://placekitten.com/700/500',
+                    'http://placekitten.com/500/600',
+                    'http://placekitten.com/500/500',
+                    'https://picsum.photos/700/500?grayscale',
+                ].map((url, index) => (
+                    <figure
+                        className="artwork col-4 col-md-2"
+                        data-selected=""
+                        data-genre="Cat"
+                        data-status="available"
+                        data-date="2020/09/25"
+                        data-title="Cat two"
+                        data-size="44"
+                        itemProp="associatedMedia"
+                        itemScope
+                        itemType="http://schema.org/ImageObject"
+                        key={index}
+                    >
+                        <a href="" itemProp="contentUrl">
+                            <img src={url} itemProp="thumbnail" />
+                        </a>
+                        <figcaption itemProp="caption description">Cat {index}</figcaption>
+                    </figure>
+                ))}
+            </Masonry> */}
+        </>
+    );
 };
 
 export default Home;

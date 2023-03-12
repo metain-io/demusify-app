e5c49de1c5f6dc4497bc02333f1251d1b5d9d602
ntchung <nguyentruongchung@gmail.com>
2023-03-12 21:19:25 +0700

    fix build errors

--- frontend/src/modules/app/pages/view-item/tabs/tab-stats/index.tsx
+++ frontend/src/modules/app/pages/view-item/tabs/tab-stats/index.tsx
@@ -9,8 +9,9 @@ export const TabStats = (props: TabStatsProps) => {
         <div className="tab-pane fade" id="stats" role="tabpanel" aria-labelledby="properties-tab">
             <div className="rounded-t-2lg rounded-b-2lg rounded-tl-none border border-jacarta-100 bg-white p-6 dark:border-jacarta-600 dark:bg-jacarta-700 md:p-10">
                 <div className="grid grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4">
-                    {item?.stats?.map((stat: any) => (
+                    {item?.stats?.map((stat: any, index: number) => (
                         <a
+                            key={index}
                             href="collection"
                             className="flex flex-col space-y-2 rounded-2lg border border-jacarta-100 bg-light-base p-5 text-center transition-shadow hover:shadow-lg dark:border-jacarta-600 dark:bg-jacarta-800"
                         >

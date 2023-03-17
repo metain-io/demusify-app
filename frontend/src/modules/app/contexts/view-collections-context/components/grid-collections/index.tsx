0ed540ecf6516693ce6ed8373a36df9ed3d18420
VTTruong287 <truong.vt287@gmail.com>
2023-03-17 18:49:28 +0700

    [SolHackathon] Update Collections Data binding

--- frontend/src/modules/app/contexts/view-collections-context/components/grid-collections/index.tsx
+++ frontend/src/modules/app/contexts/view-collections-context/components/grid-collections/index.tsx
@@ -8,20 +8,25 @@ export const GridCollections = () => {
                 <article key={index}>
                     <div className="block rounded-2.5xl border border-jacarta-900 bg-white p-[1.1875rem] transition-shadow hover:shadow-lg dark:border-jacarta-700 dark:bg-jacarta-900">
                         <figure>
-                            <a href="view-item">
+                            <a href={`view-item?itemId=${item.itemID}`}>
                                 <img
-                                    src={item.image}
+                                    src={item.coverArtImage}
                                     alt="item 1"
                                     width={230}
                                     height={230}
                                     className="w-full rounded-[0.625rem]"
                                     loading="lazy"
+                                    style={{
+                                        width: '230px',
+                                        height: '230px',
+                                        objectFit: 'cover',
+                                    }}
                                 />
                             </a>
                         </figure>
                         <div className="mt-4 flex items-center justify-between">
                             <a
-                                href="item"
+                                href={`view-item?itemId=${item.itemID}`}
                                 style={{
                                     maxWidth: '10rem',
                                     overflow: 'hidden',
@@ -34,16 +39,18 @@ export const GridCollections = () => {
                                 </span>
                             </a>
                             <span className="flex items-center whitespace-nowrap rounded-md border border-jacarta-100 py-1 px-2 dark:border-jacarta-600">
-                                <span className="text-sm font-medium tracking-tight text-green">{item.price} SOL</span>
+                                <span className="text-sm font-medium tracking-tight text-green">
+                                    {item.licenseMonetizations[0].value} SOL
+                                </span>
                             </span>
                         </div>
                         <div className="mt-2 text-sm">
-                            <span className="dark:text-jacarta-300">{item.author}</span>
+                            <span className="dark:text-jacarta-300">{item?.username?.substring(0, 12)}...</span>
                             {/* <span className="text-jacarta-700 dark:text-jacarta-100">1.3 ETH</span> */}
                         </div>
                         <div className="mt-8 flex items-center justify-between">
                             <a
-                                href="view-item"
+                                href={`view-item?itemId=${item.itemID}`}
                                 // type="button"
                                 className="font-display text-sm font-semibold text-accent"
                                 // data-bs-toggle="modal"
@@ -67,7 +74,7 @@ export const GridCollections = () => {
                                         <path d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z" />
                                     </svg>
                                 </span>
-                                <span className="text-sm dark:text-jacarta-200">{item.likeCount}</span>
+                                <span className="text-sm dark:text-jacarta-200">{item.liked || 0}</span>
                             </div>
                         </div>
                     </div>
--- frontend/src/modules/app/contexts/view-collections-context/view-collections-provider.tsx
+++ frontend/src/modules/app/contexts/view-collections-context/view-collections-provider.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { PropsWithChildren } from 'react';
 import { ViewCollectionsContext, ViewCollectionsState, ViewCollectionsStatus } from './index';
 import { collectionsData } from './data';
+import { useSelector } from 'react-redux';
+import { selectUserNftData } from '@modules/auth/redux/login/slice';
 
 export type ViewCollectionsProviderProps = PropsWithChildren<{ filterQuery?: any }>;
 
 export const ViewCollectionsProvider = (props: ViewCollectionsProviderProps) => {
     const { children, filterQuery } = props;
+    const userNFTData = useSelector(selectUserNftData)
 
     const [state, setState] = React.useState<ViewCollectionsState>({
         status: ViewCollectionsStatus.LOADING,
@@ -17,64 +20,53 @@ export const ViewCollectionsProvider = (props: ViewCollectionsProviderProps) => 
     const [collections, setCollections] = React.useState<Array<any>>([]);
 
     React.useEffect(() => {
-        // TODO: Check if need any initial process
+        if (!userNFTData.items || userNFTData.items.length == 0) return;
+
         setState({ status: ViewCollectionsStatus.LOADING });
 
-        const timeout = setTimeout(() => {
-            const fetchedCollections: Array<any> = collectionsData;
+        setState({
+            status: ViewCollectionsStatus.LOAD_SUCCEEDED,
+        });
 
-            setState({
-                status: ViewCollectionsStatus.LOAD_SUCCEEDED,
-            });
-
-            setCollections(fetchedCollections);
-        }, 2000);
-
-        return () => {
-            clearTimeout(timeout);
-        };
-    }, []);
+        setCollections(userNFTData.items.slice(0, 20));
+    }, [userNFTData]);
 
     const handleReload = async () => {
-        setState({ status: ViewCollectionsStatus.LOADING });
+        // setState({ status: ViewCollectionsStatus.LOADING });
 
-        setTimeout(() => {
-            const fetchedCollections: Array<any> = collectionsData;
+        // setState({
+        //     status: ViewCollectionsStatus.LOAD_SUCCEEDED,
+        // });
 
-            setState({
-                status: ViewCollectionsStatus.LOAD_SUCCEEDED,
-            });
-
-            setCollections(fetchedCollections);
-        }, 2000);
+        // setCollections(userNFTData.items);
     };
 
     const handleLoadingMore = async () => {
-        setState({ status: ViewCollectionsStatus.LOADING });
+        // setState({ status: ViewCollectionsStatus.LOADING });
 
-        setTimeout(() => {
-            const fetchedCollections: Array<any> = collectionsData;
+        // setTimeout(() => {
+        //     const fetchedCollections: Array<any> = collectionsData;
 
-            setState({
-                status: ViewCollectionsStatus.LOAD_SUCCEEDED,
-            });
+        //     setState({
+        //         status: ViewCollectionsStatus.LOAD_SUCCEEDED,
+        //     });
 
-            setCollections([...collections, ...fetchedCollections]);
-        }, 2000);
+        //     setCollections([...collections, ...fetchedCollections]);
+        // }, 2000);
     };
 
     const handleFilter = async (filterQuery: any) => {
-        setState({ status: ViewCollectionsStatus.LOADING, filterQuery });
+        // setState({ status: ViewCollectionsStatus.LOADING, filterQuery });
 
-        setTimeout(() => {
-            const fetchedCollections: Array<any> = collectionsData;
+        // setTimeout(() => {
+        //     const fetchedCollections: Array<any> = collectionsData;
 
-            setState({
-                status: ViewCollectionsStatus.LOAD_SUCCEEDED,
-            });
+        //     setState({
+        //         status: ViewCollectionsStatus.LOAD_SUCCEEDED,
+        //     });
 
-            setCollections(fetchedCollections);
-        }, 2000);
+        //     setCollections(fetchedCollections);
+        // }, 2000);
     };
 
     return (
